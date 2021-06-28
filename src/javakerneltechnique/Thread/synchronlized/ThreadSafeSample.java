a299f0471536f36475965b7e1f250db862ea3cfd
yaohua <1436385860@qq.com>
2021-06-28 09:15:20 +0800

    add a ThreadSafeSample

---
+++ src/javakerneltechnique/Thread/synchronlized/ThreadSafeSample.java
@@ -0,0 +1,45 @@
+package javakerneltechnique.Thread.synchronlized;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ThreadSafeSample {
+    public int sharedState;
+    public void nonSafeAction(){
+        while(sharedState<100000){
+                int former = sharedState++;
+                int latter = sharedState;
+                if (former != latter - 1) {
+                    System.out.println("Observed data race, former is " + former + ", " + "latter is " + latter);
+                }
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        ThreadSafeSample sample = new ThreadSafeSample();
+        Thread threadA = new Thread(){
+            public void run(){
+                sample.nonSafeAction();
+            }
+        };
+        Thread threadB = new Thread(){
+            public void run(){
+                sample.nonSafeAction();
+            }
+        };
+           threadA.start();
+           threadB.start();
+           threadA.join();
+           threadB.join();
+    }
+
+    public void fun(){
+        ReentrantLock fairLock = new ReentrantLock(true);
+        fairLock.lock();
+        try{
+
+        }finally {
+            fairLock.unlock();
+        }
+    }
+
+
+}
